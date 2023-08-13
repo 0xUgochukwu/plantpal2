fc8004e8c79b467b0de44f5a0c36ca0f66105b1e
lordrhaegar <sparkzilla7@gmail.com>
2023-08-13 17:35:46 +0100

    Client side Validation

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const FingerprintScreen(),
+      home: const AuthenticationWrapper(),
       routes: {
         FingerprintScreen.id: (context) => const FingerprintScreen(),
         LoginScreen.id: (context) => const LoginScreen(),
